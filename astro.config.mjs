7124e20c3235caa04fa45eca69cea5be4d71455b
iBz-04 <issakaibrahimrayamah@gmail.com>
2025-10-25 14:48:26 +0300

    chore: add seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://compa.so",
-	integrations: [UnoCSS({ injectReset: true })],
+	site: "https://offeline.site",
+	integrations: [
+		UnoCSS({ injectReset: true }),
+		sitemap(),
+	],
 });
